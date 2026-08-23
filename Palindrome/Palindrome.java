0a78dc48182f282128f9fef3f07fa43bfe9b667a
wkhlifi <wkhlifi654@gmail.com>
2026-08-23 19:00:15 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s==null){
+            return false;
+        }
           s = s.toLowerCase();
         for (int i=0; i<s.length()/2 ; i++){
             if  (s.charAt(i)!=(s.charAt(s.length() - 1 - i))){
