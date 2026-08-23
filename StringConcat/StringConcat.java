f3b0a82e44cb0a9a7ef898f1d7d8b8c122ad807d
wkhlifi <wkhlifi654@gmail.com>
2026-08-23 17:40:46 +0100

    StringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,7 +1,7 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1==null && s2==null){
-            return "";
+            return null;
         }
         if (s1==null){
             return s2;
