cd3310095e07b512762ad49995da6283897f5061
wkhlifi <wkhlifi654@gmail.com>
2026-08-23 18:06:36 +0100

    StringReplace

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,9 @@
+public class StringReplace {
+
+    public static String replace(String s, char target, char replacement) {
+        return s.replace(target, replacement);
+    }
+    public static String replace(String s, String target, String replacement) {
+        return s.replace(target, replacement);
+    }
+}
