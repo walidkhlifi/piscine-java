e4d3bcd2aa5bd9b171bac1f85806fd4626c254f0
wkhlifi <wkhlifi654@gmail.com>
2026-08-23 17:52:08 +0100

    StringContains

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,8 +1,11 @@
 public class StringContains {
+
     public static boolean isStringContainedIn(String subString, String s) {
-        if (s==null){
+
+        if (s == null) {
             return false;
         }
+
         return s.contains(subString);
     }
 }
