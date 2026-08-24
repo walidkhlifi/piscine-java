54b2d56b711be4e090b15fe88e539b97f2b7c03e
wkhlifi <wkhlifi654@gmail.com>
2026-08-24 12:20:19 +0100

    DoOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,17 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args==null){
+            return null;
+        }
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[2]);
+        switch(args[1]){
+            case "+": return String.valueOf(a+b);
+            case "-": return String.valueOf(a-b);
+            case "*": return String.valueOf(a*b);
+            case "/": if (b==0){return "Error";}return String.valueOf(a/b);
+            case "%": if (b==0){return "Error";}return String.valueOf(a%b);
+            default: return "Error";
+        }
+    }
+}
