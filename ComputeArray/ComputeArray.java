1ffde4b60dbe9ed3550757bf96bf22df9baff62e
wkhlifi <wkhlifi654@gmail.com>
2026-08-24 11:51:49 +0100

    ComputeArray

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,26 +1,23 @@
-public class ComputeArray{
-    public static int[] computeArray(int[] array){
+public class ComputeArray {
+
+    public static int[] computeArray(int[] array) {
+
         int[] res = new int[array.length];
-        int index=0;
-        for (int i :array){
-            if (i%3==0){
-                res[index]=(i*5);
-                index++;
-                continue;
-            }
-            if ((i-1)%3==0){
-                res[index]=(i+7);
-                index++;
-                continue;
-            }
-            if ((i-2)%3==0){
+        int index = 0;
 
-                res[index]=(i);
-                index++;
-                continue;
+        for (int i : array) {
+
+            if (i % 3 == 0) {
+                res[index] = i * 5;
+            } else if ((i - 1) % 3 == 0) {
+                res[index] = i + 7;
+            } else {
+                res[index] = i;
             }
 
+            index++;
         }
+
         return res;
     }
 }
