e2e4fbb668769f622677d818aa89e54d603fd237
wkhlifi <wkhlifi654@gmail.com>
2026-08-24 14:59:24 +0100

    cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -6,9 +6,9 @@ public class Cat {
             return;
         }
         BufferedReader reader=new BufferedReader(new FileReader(args[0]));
-        String line;
-        while((line=reader.readLine())!=null){
-            System.out.println(line);
+        int c;
+        while((c=reader.read())!=-1){
+            System.out.print(char c);
         }
         reader.close();
     }
