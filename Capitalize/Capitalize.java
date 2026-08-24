7b2023a35c18d1c3f21a11ab8bb98292ed9159bb
wkhlifi <wkhlifi654@gmail.com>
2026-08-24 15:53:28 +0100

    Capitalize

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        FileInputStream reader=new FileInputStream(args[0]);
+        FileOutputStream file = new FileOutputStream(args[1]);
+
+        String text;
+        text =new String(reader.readAllBytes());
+        text=text.trim();
+        String[] arr=text.split("\\s+");
+
+        for (int i =0; i<arr.length; i++){
+            arr[i] = Character.toUpperCase(arr[i].charAt(0))  + arr[i].substring(1);
+        }
+        String res=String.join(" ",arr);
+        file.write(res.getBytes());
+        reader.close();
+        file.close();
+    }
+}
