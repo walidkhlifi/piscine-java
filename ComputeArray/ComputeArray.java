d8a69f9114442859cdc15dc8aab5b543a1769c05
wkhlifi <wkhlifi654@gmail.com>
2026-08-24 11:54:21 +0100

    ComputeArray

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,7 +1,9 @@
 public class ComputeArray {
 
     public static int[] computeArray(int[] array) {
-
+        if array==null{
+            return null
+        }
         int[] res = new int[array.length];
         int index = 0;
 
