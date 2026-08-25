fc22bb7bef4e939fab99cab80779920c0d852eec
wkhlifi <wkhlifi654@gmail.com>
2026-08-25 16:25:40 +0100

    StarGetters

--- StarGetters/CelestialObject.java
+++ StarGetters/CelestialObject.java
@@ -17,33 +17,33 @@ public class CelestialObject{
         this.name=s;
     }
 
-    public void setX(double ex){
-        this.x=ex;
-    }
     public double getX(){
         return this.x;
     }
-
-    public void setY(double ey){
-        this.x=ey;
+    public void setX(double ex){
+        this.x=ex;
     }
+
     public double getY(){
         return this.y;
     }
-
-    public void setZ(double ez){
-        this.x=ez;
+    public void setY(double ey){
+        this.y=ey;
     }
+
     public double getZ(){
         return this.z;
     }
-
-    public void setName(String ename){
-        this.name=ename;
+    public void setZ(double ez){
+        this.z=ez;
     }
+
     public String getName(){
         return this.name;
     }
-
+    public void setName(String ename){
+        this.name=ename;
+    }
+    
     
 }
