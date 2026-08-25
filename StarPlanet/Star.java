ee3b94fe1e8aec494fc77a58fae1d634bd3d1484
wkhlifi <wkhlifi654@gmail.com>
2026-08-25 19:05:01 +0100

    StarPlanet

---
+++ StarPlanet/Star.java
@@ -0,0 +1,34 @@
+public class Star extends CelestialObject{
+    private double magnitude;
+
+    public double getMagnitude(){
+        return this.magnitude;
+    }
+    public void setMagnitude(double x){
+        magnitude=x;
+    }
+    public Star(){     
+    }
+    public Star(String name,double x,double y,double z,double magnitude){
+        super(name,x,y,z);
+        this.magnitude=magnitude;        
+    }
+    @Override
+    public int hashCode(){
+         return java.util.Objects.hash(super.hashCode(),this.magnitude);
+    }
+    @Override
+    public boolean equals(Object object){
+        if (!(object instanceof Star)){
+            return false;
+        } 
+        Star o = (Star) object;
+        return super.equals(o) && this.magnitude==o.magnitude;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s shines at the %.3f magnitude",this.name,this.magnitude);
+    }
+
+
+}
