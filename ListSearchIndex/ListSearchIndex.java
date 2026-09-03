c0b9eda3084a424988098ceb1b29a0eb8027f47e
wkhlifi <wkhlifi654@gmail.com>
2026-09-03 15:13:25 +0100

    ListSearchIndex

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -19,16 +19,14 @@ public class ListSearchIndex {
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         List<Integer> res=new ArrayList<Integer>();
         if (list == null ){
-            return null;
+            return res;
         }
         for (int i=0 ; i<list.size(); i++){
             if (list.get(i)== value){
                 res.add(i);
             }
         }
-        // if (res.isEmpty()){
-        //     return new ArrayList<Integer>();
-        // }
+
         return res;
     }
 }
