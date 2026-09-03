7da10de1070cf4e09661e21acb7d7d15ca93b13e
wkhlifi <wkhlifi654@gmail.com>
2026-09-03 16:22:28 +0100

    MapEquals

---
+++ MapEquals/MapEquals.java
@@ -0,0 +1,7 @@
+import java.util.Map;
+
+public class MapEquals {
+    public static boolean areMapsEqual(Map<String, Integer> map1, Map<String, Integer> map2) {
+       return  map1.equals(map2);
+    }
+}
