8d6b9ead66a1004d1261e43393b6eb2e3f02480c
wkhlifi <wkhlifi654@gmail.com>
2026-09-03 15:46:55 +0100

    SortList

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -1,15 +1,18 @@
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        list.sort(Comparator.naturalOrder());
-        return list;
+        List<Integer> res=new ArrayList<Integer>(list);
+        res.sort(Comparator.naturalOrder());
+        return res;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-      list.sort(Comparator.reverseOrder());
-        return list;
+        List<Integer> res=new ArrayList<Integer>(list);
+          res.sort(Comparator.reverseOrder());
+        return res;
     }
 }
