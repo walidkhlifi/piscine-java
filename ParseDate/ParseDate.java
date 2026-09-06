f35e1382de500ecb5a703c6507bc5ddf49790fc3
wkhlifi <wkhlifi654@gmail.com>
2026-09-06 17:55:26 +0100

    ParseDate

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -6,12 +6,19 @@ import java.util.Locale;
 import java.time.format.DateTimeFormatterBuilder;
 
 public class ParseDate {
+     
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null) {
+        return null;
+    }
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null) {
+        return null;
+    }
             DateTimeFormatter f = new DateTimeFormatterBuilder()
             .parseCaseInsensitive()
             .appendPattern("EEEE dd MMMM yyyy")
@@ -20,6 +27,9 @@ public class ParseDate {
     }
 
    public static LocalTime parseTimeFormat(String stringDate) {
+    if (stringDate == null) {
+        return null;
+    }
     String[] parts = stringDate.split(" ");
     
     int hour = Integer.parseInt(parts[0]) + 12;
