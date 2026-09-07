25e830ea913b2a1d8d7e039d0868dd03f4c00798
wkhlifi <wkhlifi654@gmail.com>
2026-09-07 17:26:21 +0100

    FormatDate

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -6,17 +6,26 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime==null){
+            return null;
+        }
         DateTimeFormatter f = DateTimeFormatter.ofPattern("'Le' dd MMM 'de l'an' yyyy 'à' HH'h'mm'm' 'et' ss's'",Locale.FRANCE);
         return dateTime.format(f);
     }
 
     public static String formatSimple(LocalDate date) {
+        if (dateTime==null){
+            return null;
+        }
         DateTimeFormatter f= DateTimeFormatter.ofPattern("MMMM dd yy" , Locale.ITALIC);
          return date.format(f);
 
     }
 
     public static String formatIso(LocalTime time) {
+        if (dateTime==null){
+            return null;
+        }
         return time.toString();
     }
 
