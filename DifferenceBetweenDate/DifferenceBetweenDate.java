726f70a61c785a3a62ad8ba3265975efcbcf7daf
wkhlifi <wkhlifi654@gmail.com>
2026-09-07 18:52:36 +0100

    diff

---
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -0,0 +1,26 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class DifferenceBetweenDate {
+
+    public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        return Duration.between(localTime1,localTime2).abs();
+    }
+
+    public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+         if (date1.isBefore(date2)){
+            return Period.between(date1, date2);
+        }else{
+            return Period.between(date2, date1);
+        }
+    }
+
+    public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        return Math.abs(ChronoUnit.HOURS.between(dateTime1, dateTime2));
+    }
+
+}
